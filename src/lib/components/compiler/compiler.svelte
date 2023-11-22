a040834934537cd33816651d0efc401e9680dca9
Dmitrii <dimitrii.stepanidenko@gmail.com>
2023-11-22 12:58:36 +0300

    Fixes

--- src/lib/components/compiler/compiler.svelte
+++ src/lib/components/compiler/compiler.svelte
@@ -3,19 +3,12 @@
 	import { compile_program, ParserErrror } from '$lib/pkg/cpu_sim_rs';
 	import { createEventDispatcher } from 'svelte';
 	import { addToast } from '$lib/Toaster.svelte';
-	import { AceEditor } from 'svelte-ace';
-	import 'brace/mode/json';
-	import 'brace/theme/chrome';
 	import CodeMirror from 'svelte-codemirror-editor';
-	import CommandMemory from '../command_memory/CommandMemory.svelte';
 
 	const dispatch = createEventDispatcher();
-	let textarea;
 	let error = false;
 	$: rows = countNewLines(text);
 	let numbers;
-	let minHeight = 10;
-	let maxHeight = 80;
 	let highlitedNumber: undefined | number = undefined;
 
 	$: {
@@ -108,91 +101,14 @@
 
 <div class="main">
 	<div class="main_input">
-    <CodeMirror bind:value={text} />
-		<!--<AceEditor
-			on:selectionChange={(obj) => console.log(obj.detail)}
-			on:paste={(obj) => console.log(obj.detail)}
-			on:input={(obj) => console.log(obj.detail)}
-			on:focus={() => console.log('focus')}
-			on:documentChange={(obj) => console.log(`document change : ${obj.detail}`)}
-			on:cut={() => console.log('cut')}
-			on:cursorChange={() => console.log('cursor change')}
-			on:copy={() => console.log('copy')}
-			on:init={(editor) => console.log(editor.detail)}
-			on:commandKey={(obj) => console.log(obj.detail)}
-			on:changeMode={(obj) => console.log(`change mode : ${obj.detail}`)}
-			on:blur={() => console.log('blur')}
-			width="400px"
-			height="800px"
-			theme="chrome"
-			value={text}
-      />-->
-		<!--<div class="numbers">
-			{#each numbers as number}
-				{#if error && number === highlitedNumber}
-					<div class="row" style="background-color: red;color: white;">{number}</div>
-				{:else}
-					<div class="row">{number}</div>
-				{/if}
-			{/each}
-		</div>
-		<div class="container">-->
-		<!--<pre
-				aria-hidden="true"
-        style="min-height: {minHeight}; max-height: {maxHeight}; min-width: 100px;">{text + '\n'}</pre>-->
-		<!--<textarea
-				autocomplete="off"
-				autocorrect="off"
-				autocapitalize="off"
-				spellcheck="false"
-				bind:value={text}
-				bind:this={textarea}
-				rows={80}
-				cols={80}
-				style="min-height: {minHeight}; max-height: {maxHeight}; min-width: 100px;"
-			/>
-    </div>-->
+		<CodeMirror bind:value={text} />
 	</div>
 	<button on:click={() => compile()}>Compile</button>
 </div>
 
 <style>
-	.main {
-	}
 	.main_input {
 		display: flex;
 		font-size: 14px;
-	}
-	.container {
-		position: relative;
-		font-size: 14px;
-		padding-left: 5px;
-	}
-	.numbers {
-		margin-top: 0.5em;
-	}
-
-	pre,
-	textarea {
-		font-family: inherit;
-		padding: 0.5em;
-		box-sizing: border-box;
-		border: 1px solid #eee;
-		line-height: 1.2;
-
-		width: 100%;
-		min-width: 300px;
-		height: 100%;
-		top: 0;
-		overflow: hidden;
-	}
-
-	textarea {
-		position: absolute;
-		width: 100%;
-		min-width: 100px;
-		height: 100%;
-		top: 0;
-		resize: none;
 	}
 </style>
