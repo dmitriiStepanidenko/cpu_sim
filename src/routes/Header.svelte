a5d262503184de127bfdce1c779f515895991aa5
Dmitrii <dimitrii.stepanidenko@gmail.com>
2023-11-22 22:36:36 +0300

    Fix enhanced:img

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,9 +1,9 @@
 <script>
-	import Logo from '$lib/images/android-chrome-512x512.png';
+	import Logo from '$lib/images/android-chrome-512x512.png?enhanced';
 </script>
 
 <div class="main">
-	<enchanched:img src={Logo} width="64" height="64" alt="NN generated cpu" class="logo" />
+	<enhanced:img src={Logo} alt="NN generated cpu" class="logo" sizes="64px 64px"/>
 	<h1 class="main_title">Simulator of imaginary Harvard-architecture cpu</h1>
 </div>
 
@@ -21,6 +21,8 @@
   .logo {
     padding-right: 0px;
     padding-left: 0px;
+    width: 64px;
+    height: 64px;
   }
 	.main_title {
 		font-size: 32px;
