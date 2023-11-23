861c57a48a821208feb321284ab2fc01ce0f6b93
Dmitrii <dimitrii.stepanidenko@gmail.com>
2023-11-24 01:45:40 +0300

    Updates

---
+++ _app/immutable/chunks/paths.8f127919.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1w04jte?.base??"/sveltekit-github-pages",e=globalThis.__sveltekit_1w04jte?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/paths.f04588ef.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_sqg05z?.base??"/sveltekit-github-pages",e=globalThis.__sveltekit_sqg05z?.assets??s;export{e as a,s as b};
